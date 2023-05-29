3d5916fb7721495840329d10e3db7b1c71b9b834
Aristo <darkvale57@gmail.com>
2023-05-29 17:39:13 -0400

    First Test Case

--- phptravel/phptravel.py
+++ phptravel/phptravel.py
@@ -5,20 +5,59 @@ import os
 import phptravel.constants as const
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait 
+import selenium.webdriver.support.expected_conditions as EC
 import time
 
-class Phptravel(webdriver.Chrome):
+import unittest
+
+class Phptravel(webdriver.Chrome, unittest.TestCase):
     def __init__(self, driver_path=r"C:\Program Files (x86)", teardown=False):
         self.driver_path = driver_path
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Phptravel, self).__init__()
+        self.implicitly_wait(10)
+        self.maximize_window()
+
     
     def __exit__(self, exc_type, exc_val, exc_tb):
+        time.sleep(20)
         if self.teardown:
             self.quit()
+        
 
     def go_first_page(self):
         self.get(const.BASE_URL)
-        #self.quit
-        time.sleep(5)
+
+    def enter_object(username_box, text, element):
+        fname_text = username_box.find_element(By.NAME, element)
+        fname_text.click()
+        fname_text.send_keys(text)
+    
+    def enter_form(self, fname, lname, bname, email):
+        
+        username_box = self.find_element(By.CLASS_NAME, "form")
+        Phptravel.enter_object(username_box, fname, "first_name")
+        Phptravel.enter_object(username_box, lname, "last_name")
+        Phptravel.enter_object(username_box, bname, "business_name")
+        Phptravel.enter_object(username_box, email, "email")
+        num1 = username_box.find_element(By.ID, "numb1").text
+        num2 = username_box.find_element(By.ID, "numb2").text
+        
+        
+        equals = username_box.find_element(By.ID, "number")
+        equals.click()
+        equals.send_keys(int(num1) + int(num2))
+        username_box.find_element(By.ID, "demo").click()
+
+        checkbox = self.find_element(By.CLASS_NAME, "completed")
+        self.assertIn("Thank you!", checkbox.find_element(By.CSS_SELECTOR, "h2").text)
+        
+        
+
+
+
+
+    
+
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from phptravel.phptravel import Phptravel
 
+
 with Phptravel(teardown=True) as bot:
     bot.go_first_page()
+    print("asserting working form for first name: Nick, last name: Cage, business name: Dior, email nickcage@gmail.com")
+    bot.enter_form("Nick", "Cage", "Dior", "nickcage@gmail.com")
     print('Exiting ...')
